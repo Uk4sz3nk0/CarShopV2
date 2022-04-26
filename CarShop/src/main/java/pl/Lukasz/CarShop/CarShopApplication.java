6fcf2467f42b0fce96ba36430e52172ae00a4423
Uk4sz3nk0 <gerdzio53@gmail.com>
2022-04-26 15:24:26 +0200

    Project init

---
+++ CarShop/src/main/java/pl/Lukasz/CarShop/CarShopApplication.java
@@ -0,0 +1,13 @@
+package pl.Lukasz.CarShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShopApplication.class, args);
+	}
+
+}
---
+++ carshopui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          <h1 class="upperCase">There's nothing</h1>
+          <h3>Come back in the future</h3>
+        </div>
+    </div>
+  );
+}
+
+export default App;
