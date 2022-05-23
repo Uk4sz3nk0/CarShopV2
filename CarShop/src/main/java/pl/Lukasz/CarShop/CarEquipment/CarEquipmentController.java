20750204f1de5af4f018574c9a9f2abf2859eea1
Uk4sz3nk0 <gerdzio53@gmail.com>
2022-05-23 14:20:21 +0200

    Fixing glitches

--- CarShop/src/main/java/pl/Lukasz/CarShop/CarEquipment/CarEquipmentController.java
+++ CarShop/src/main/java/pl/Lukasz/CarShop/CarEquipment/CarEquipmentController.java
@@ -1,14 +1,24 @@
 package pl.Lukasz.CarShop.CarEquipment;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/car")
 public class CarEquipmentController {
 
     private final CarEquipmentService carEquipmentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarEquipment> findById(@PathVariable Long id) {
+        return carEquipmentService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
--- CarShop/src/main/java/pl/Lukasz/CarShop/ExpandedCar/ExpandedCarRepository.java
+++ CarShop/src/main/java/pl/Lukasz/CarShop/ExpandedCar/ExpandedCarRepository.java
@@ -2,9 +2,13 @@ package pl.Lukasz.CarShop.ExpandedCar;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ExpandedCarRepository extends JpaRepository<ExpandedCar, Long> {
 
+    Optional<ExpandedCar> findById(@Param("id") Long id);
 }
